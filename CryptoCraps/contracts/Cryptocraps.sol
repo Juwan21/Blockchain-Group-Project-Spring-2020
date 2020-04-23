3f66e02b9b0de9f4945f7890edb30a0d82539ac7
bcarter46 <57848062+bcarter46@users.noreply.github.com>
2020-04-23 18:04:31 -0400

    Update Cryptocraps.sol

--- CryptoCraps/contracts/Cryptocraps.sol
+++ CryptoCraps/contracts/Cryptocraps.sol
@@ -2,73 +2,6 @@ pragma solidity ^0.4.24;
 
 contract CryptoCraps{
 	
-	struct games{
-		uint8 setPoint;
-		uint8 rollResult;
-		uint8 betAmount;
-		uint numberOfRolls;
-		bool isBetSet;
-	}
-
-	mapping(address => games) private games;
-
-	event newSetPoint(address bidder, uint8 setPoint);
-	event newBetSet(address bidder, uint8 betAmount);
-	event rollResult(address bidder, uint8 rollResult);
-
-
-    uint8 private randomizer;
-
-	constructor() public{
-		randomizer=5;
-	}
-
-	function isBetSet() public view returns(bool){
-		return games[msg.sender].isBetSet;
-	}
-
-	function getNewbet(userInput) public returns(uint8){
-		require(games[msg.sender].isBetSet == false);
-		games[msg.sender].isBetSet = true;
-		//line below should probably be changed to allow user to select the number they wish to betting on.
-		games[msg.sender].setPoint = random() ;
-		//line below this is intended to set the new bet amount. not sure how to accept user input so this is currently not correct.
-		games[msg.sender].betAmount=userInput;
-		randomizer += random();
-		emit NewBetSet(msg.sender,games[msg.sender].setPoint);
-		emit newBetAmount(msq.sender,games[msg.sender].betAmount);
-		return games[msg.sender].setPoint;
-	}
-
-	function random() private view returns (uint8) {
-       	uint256 blockValue = uint256(blockhash(block.number-1 + block.timestamp));
-        blockValue = blockValue + uint256(randomFactor);
-        return uint8(blockValue % 5) + 1;
-    }
-
-    function roll(){
-    	require (games[msg.sender].isBetSet == true);
-    	randomizer += random();
-    	if(games[msg.sender].numberOfRolls > 3){
-    		return loser;
-    	}
-    	games[msg.sender].rollResult = random();
-    	emit rollResult(msg.sender, games[msg.sender].rollResult);
-    	if(games[msg.sender].rollResult == 7 || games[msg.sender].rollResult == games[msg.sender].setPoint){
-    		games[msg.sender].numberOfRolls = 0;
-    		return winner;
-    	}
-    	if(games[msg.sender].rollResult == 2 || games[msg.sender].rollResult==3 || games[msg.sender.rollResult==12]){
-		    games[msg.sender].numberOfRolls = 0;
-    		return loser;
-    	}
-    	else{
-    		games[msg.sender].numberOfRolls = games[msg.sender].numberOfRolls + 1;
-    		roll();
-    	}pragma solidity ^0.4.24;
-
-contract CryptoCraps{
-	
 	struct game{
 		uint8 setPoint;
 		uint8 rollResult;
@@ -95,53 +28,73 @@ contract CryptoCraps{
 	function isBetSet() public view returns(bool){
 		return games[msg.sender].isBetSet;
 	}
+	
+	function getSetPoint() public view returns(uint8){
+	    require(games[msg.sender].isBetSet == true);
+	    return games[msg.sender].setPoint;
+	}
+	function getGameResult() public view returns(string){
+	    return games[msg.sender].gameResult;
+	}
 
 	function getNewbet(uint8 userInput) public returns(uint8){
 		require(games[msg.sender].isBetSet == false);
 		games[msg.sender].numberOfRolls=0;
 		games[msg.sender].isBetSet = true;
-		//line below should probably be changed to allow user to select the number they wish to betting on.
-		games[msg.sender].setPoint = random() ;
-		//line below this is intended to set the new bet amount. not sure how to accept user input so this is currently not correct.
+		games[msg.sender].setPoint = getrandom();
 		games[msg.sender].betAmount=userInput;
-		randomizer += random();
+		randomizer += getrandom();
 		emit newSetPoint(msg.sender,games[msg.sender].setPoint);
 		emit newBetSet(msg.sender,games[msg.sender].betAmount);
 		return games[msg.sender].setPoint;
 	}
 
-	function random() private view returns (uint8) {
-       	uint256 blockValue = uint256(blockhash(block.number-1 + block.timestamp));
-        blockValue = blockValue + uint256(randomizer);
-        return uint8(blockValue % 5) + 1;
-    }
+	function getrandom() public view returns (uint8) {
+       uint8 value= uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))*uint256(msg.sender)%5+1);
+       uint8 value1= uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%5+1);
+       return value1+value;
+   }
 
     function roll() public returns(bool){
     	require (games[msg.sender].isBetSet == true);
-    	randomizer += random();
+    	randomizer += getrandom();
+    	if(games[msg.sender].setPoint == 7 || games[msg.sender].setPoint == 11){
+    	    games[msg.sender].numberOfRolls = 0;
+    	    games[msg.sender].isBetSet=false;
+    	    games[msg.sender].gameResult="Winner";
+    	    return true;
+    	}
     	if(games[msg.sender].numberOfRolls > 3){
-    		games[msg.sender].gameResult="Winner";
+    	    games[msg.sender].numberOfRolls = 0;
+    	    games[msg.sender].isBetSet=false;
+    		games[msg.sender].gameResult="Loser";
     		return false;
     	}
-    	games[msg.sender].rollResult = random();
+    	if(games[msg.sender].setPoint == 2 || games[msg.sender].setPoint==3 || games[msg.sender].setPoint==12){
+		    games[msg.sender].numberOfRolls = 0;
+		    games[msg.sender].isBetSet=false;
+    		games[msg.sender].gameResult="Loser";
+    		return false;
+    		
+    	}
+    	games[msg.sender].rollResult = getrandom();
+    	games[msg.sender].numberOfRolls = games[msg.sender].numberOfRolls + 1;
     	emit rollResult(msg.sender, games[msg.sender].rollResult);
-    	if(games[msg.sender].rollResult == 7 || games[msg.sender].rollResult == games[msg.sender].setPoint){
+    	if(games[msg.sender].rollResult == games[msg.sender].setPoint){
     		games[msg.sender].numberOfRolls = 0;
     		games[msg.sender].isBetSet=false;
     		games[msg.sender].gameResult="Winner";
     		return true;
     		
     	}
-    	if(games[msg.sender].rollResult == 2 || games[msg.sender].rollResult==3 || games[msg.sender].rollResult==12){
-		    games[msg.sender].numberOfRolls = 0;
-		    games[msg.sender].isBetSet=false;
-    		games[msg.sender].gameResult="Winner";
-    		return false;
-    		
+    	if(games[msg.sender].rollResult == 7 ){
+    	   games[msg.sender].numberOfRolls = 0;
+    	    games[msg.sender].isBetSet=false;
+    		games[msg.sender].gameResult="Loser";
+    		return false; 
     	}
-    	else{
-    		games[msg.sender].numberOfRolls = games[msg.sender].numberOfRolls + 1;
-    		roll();
-    	}
+    
+    	games[msg.sender].numberOfRolls = games[msg.sender].numberOfRolls + 1;
+    	roll();
     }
 }
