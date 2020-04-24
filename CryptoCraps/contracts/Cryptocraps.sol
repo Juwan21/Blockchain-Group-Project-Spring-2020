b4c84bc145b118523c13adcd7061f9302e330667
bcarter46 <57848062+bcarter46@users.noreply.github.com>
2020-04-24 13:03:17 -0400

    Update Cryptocraps.sol

--- CryptoCraps/contracts/Cryptocraps.sol
+++ CryptoCraps/contracts/Cryptocraps.sol
@@ -7,7 +7,7 @@ contract CryptoCraps{
 		uint8 rollResult;
 		uint8 betAmount;
 		uint numberOfRolls;
-		string gameResult;
+		bool gameResult;
 		bool isBetSet;
 	}
 
@@ -33,12 +33,25 @@ contract CryptoCraps{
 	    require(games[msg.sender].isBetSet == true);
 	    return games[msg.sender].setPoint;
 	}
-	function getGameResult() public view returns(string){
+	function getGameResult() public view returns(bool){
 	    return games[msg.sender].gameResult;
+	}
+	
+	function sendPot() public payable{
+	   address _reciever=msg.sender;
+	    require(games[msg.sender].gameResult == true);
+	    _reciever.transfer(games[msg.sender].betAmount);
+	}
+	
+	function getPot() public payable{
+	    address _reciever=msg.sender;
+	    require(games[msg.sender].gameResult == false);
+	    _reciever.transfer(games[msg.sender].betAmount);
 	}
 
 	function getNewbet(uint8 userInput) public returns(uint8){
 		require(games[msg.sender].isBetSet == false);
+		require(msg.sender.balance > userInput);
 		games[msg.sender].numberOfRolls=0;
 		games[msg.sender].isBetSet = true;
 		games[msg.sender].setPoint = getrandom();
@@ -61,19 +74,19 @@ contract CryptoCraps{
     	if(games[msg.sender].setPoint == 7 || games[msg.sender].setPoint == 11){
     	    games[msg.sender].numberOfRolls = 0;
     	    games[msg.sender].isBetSet=false;
-    	    games[msg.sender].gameResult="Winner";
+    	    games[msg.sender].gameResult=true;
     	    return true;
     	}
-    	if(games[msg.sender].numberOfRolls > 3){
+    	if(games[msg.sender].numberOfRolls >= 3){
     	    games[msg.sender].numberOfRolls = 0;
     	    games[msg.sender].isBetSet=false;
-    		games[msg.sender].gameResult="Loser";
+    		games[msg.sender].gameResult=false;
     		return false;
     	}
     	if(games[msg.sender].setPoint == 2 || games[msg.sender].setPoint==3 || games[msg.sender].setPoint==12){
 		    games[msg.sender].numberOfRolls = 0;
 		    games[msg.sender].isBetSet=false;
-    		games[msg.sender].gameResult="Loser";
+    		games[msg.sender].gameResult=false;
     		return false;
     		
     	}
@@ -83,14 +96,14 @@ contract CryptoCraps{
     	if(games[msg.sender].rollResult == games[msg.sender].setPoint){
     		games[msg.sender].numberOfRolls = 0;
     		games[msg.sender].isBetSet=false;
-    		games[msg.sender].gameResult="Winner";
+    		games[msg.sender].gameResult=true;
     		return true;
     		
     	}
     	if(games[msg.sender].rollResult == 7 ){
     	   games[msg.sender].numberOfRolls = 0;
     	    games[msg.sender].isBetSet=false;
-    		games[msg.sender].gameResult="Loser";
+    		games[msg.sender].gameResult=true;
     		return false; 
     	}
     
