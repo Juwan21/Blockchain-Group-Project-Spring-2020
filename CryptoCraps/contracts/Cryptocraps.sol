a09cea74761d0f9a3672aaae44b74290dcfc5cbc
bcarter46 <57848062+bcarter46@users.noreply.github.com>
2020-04-26 16:29:39 -0400

    Update Cryptocraps.sol

--- CryptoCraps/contracts/Cryptocraps.sol
+++ CryptoCraps/contracts/Cryptocraps.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-contract CryptoCraps{
+contract cryptocraps{
 	
 	struct game{
 		uint8 setPoint;
@@ -23,6 +23,7 @@ contract CryptoCraps{
 
 	constructor() public{
 		randomizer=5;
+
 	}
 
 	function isBetSet() public view returns(bool){
@@ -37,32 +38,37 @@ contract CryptoCraps{
 	    return games[msg.sender].gameResult;
 	}
 	
-	function sendPot() public payable{
+	function sendPayment() public payable{
 	   address _reciever=msg.sender;
 	    require(games[msg.sender].gameResult == true);
 	    _reciever.transfer(games[msg.sender].betAmount);
 	}
 	
-	function getPot() public payable{
+	function recievePayment() public payable{
 	    address _reciever=msg.sender;
 	    require(games[msg.sender].gameResult == false);
 	    _reciever.transfer(games[msg.sender].betAmount);
 	}
 
-	function getNewbet(uint8 userInput) public returns(uint8){
+	function getNewBet(uint8 userInput) public{
 		require(games[msg.sender].isBetSet == false);
 		require(msg.sender.balance > userInput);
 		games[msg.sender].numberOfRolls=0;
 		games[msg.sender].isBetSet = true;
-		games[msg.sender].setPoint = getrandom();
+		
 		games[msg.sender].betAmount=userInput;
-		randomizer += getrandom();
-		emit newSetPoint(msg.sender,games[msg.sender].setPoint);
+		randomizer += getRandom();
+		
 		emit newBetSet(msg.sender,games[msg.sender].betAmount);
-		return games[msg.sender].setPoint;
+		
 	}
-
-	function getrandom() public view returns (uint8) {
+    function setThePoint() public returns(uint8){
+		require(games[msg.sender].setPoint == 0);
+        games[msg.sender].setPoint = getRandom();
+        emit newSetPoint(msg.sender,games[msg.sender].setPoint);
+        return games[msg.sender].setPoint;
+    }
+	function getRandom() public view returns (uint8) {
        uint8 value= uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))*uint256(msg.sender)%5+1);
        uint8 value1= uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%5+1);
        return value1+value;
@@ -70,33 +76,37 @@ contract CryptoCraps{
 
     function roll() public returns(bool){
     	require (games[msg.sender].isBetSet == true);
-    	randomizer += getrandom();
+    	randomizer += getRandom();
     	if(games[msg.sender].setPoint == 7 || games[msg.sender].setPoint == 11){
     	    games[msg.sender].numberOfRolls = 0;
     	    games[msg.sender].isBetSet=false;
     	    games[msg.sender].gameResult=true;
+			games[msg.sender].setPoint=0;
     	    return true;
     	}
     	if(games[msg.sender].numberOfRolls >= 3){
     	    games[msg.sender].numberOfRolls = 0;
     	    games[msg.sender].isBetSet=false;
     		games[msg.sender].gameResult=false;
+			games[msg.sender].setPoint=0;
     		return false;
     	}
     	if(games[msg.sender].setPoint == 2 || games[msg.sender].setPoint==3 || games[msg.sender].setPoint==12){
 		    games[msg.sender].numberOfRolls = 0;
 		    games[msg.sender].isBetSet=false;
     		games[msg.sender].gameResult=false;
+			games[msg.sender].setPoint=0;
     		return false;
     		
     	}
-    	games[msg.sender].rollResult = getrandom();
+    	games[msg.sender].rollResult = getRandom();
     	games[msg.sender].numberOfRolls = games[msg.sender].numberOfRolls + 1;
     	emit rollResult(msg.sender, games[msg.sender].rollResult);
     	if(games[msg.sender].rollResult == games[msg.sender].setPoint){
     		games[msg.sender].numberOfRolls = 0;
     		games[msg.sender].isBetSet=false;
     		games[msg.sender].gameResult=true;
+			games[msg.sender].setPoint=0;
     		return true;
     		
     	}
@@ -104,6 +114,7 @@ contract CryptoCraps{
     	   games[msg.sender].numberOfRolls = 0;
     	    games[msg.sender].isBetSet=false;
     		games[msg.sender].gameResult=true;
+			games[msg.sender].setPoint=0;
     		return false; 
     	}
     
